b7f4df44a160d5dfe5c46889d468f1980a7b9d9c
Erkin Eren <ernerkin@gmail.com>
2019-05-02 00:22:49 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+require 'simple_html_dom.php';
+use Curl\Curl;
+
+// initialize curl
+// you can install via "composer require php-curl-class/php-curl-class"
+$curl = new Curl();
+
+// set cookies
+$curl->setCookieFile(__DIR__ . '/cookies.txt');
+$curl->setCookieJar(__DIR__ . '/cookies.txt');
+
+// decode gzip encoded because amazon is using gzip
+$curl->setOpt(CURLOPT_ENCODING , "gzip");
+
+// set request header like a browser
+$curl->setHeaders([
+    'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'accept-encoding' => 'gzip, deflate, br',
+    'accept-language' => 'en,tr;q=0.9',
+    'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+]);
+
+// request
+$curl->get('https://www.amazon.com/Apple-iPhone-Plus-Unlocked-32GB/dp/B01N6ZAR0D/');
+
+// get raw response
+$response = $curl->getRawResponse();
+
+// parser
+$html = new simple_html_dom();
+
+// load from string html
+$html->load($response);
+
+// find price and print
+$price = $html->find('#price', 0)->plaintext;
+echo $price;
